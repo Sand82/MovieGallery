d5e8cc282a57f7a129c2acdf1e9363a328c01417
Sand82 <sandoki@abv.bg>
2022-06-29 19:11:56 +0300

    added template separate section

---
+++ movie-gallery/src/component/Scroll.js
@@ -0,0 +1,3 @@
+export default function Scroll () {
+    return <a className="scroll-top disabled" href="#"><i className="fas fa-angle-up" aria-hidden="true"></i></a>
+}
---
+++ movie-gallery/src/component/TopRatedMovies.js
@@ -0,0 +1,148 @@
+export default function TopRatedMovies () {
+    return <section className="section-long">
+    <div className="container">
+        <div className="section-head">
+            <h2 className="section-title text-uppercase">Latest news</h2>
+        </div>
+        <div className="grid row">
+            <div className="col-md-6">
+                <article className="article-tape-entity">
+                    <a className="entity-preview" href="article-sidebar-right.html" data-role="hover-wrap">
+                        <span className="embed-responsive embed-responsive-16by9">
+                            <img className="embed-responsive-item" src="http://via.placeholder.com/720x405" alt="" />
+                        </span>
+                        <span className="entity-date">
+                            <span className="tape-block tape-horizontal tape-single bg-theme text-white">
+                                <span className="tape-dots"></span>
+                                <span className="tape-content m-auto">20 jul 2019</span>
+                                <span className="tape-dots"></span>
+                            </span>
+                        </span>
+                        <span className="d-over bg-black-80 collapse animated faster" data-show-classname="fadeIn show" data-hide-classname="fadeOut show">
+                            <span className="m-auto"><i className="fas fa-search"></i></span>
+                        </span>
+                    </a>
+                    <div className="entity-content">
+                        <h4 className="entity-title">
+                            <a className="content-link" href="article-sidebar-right.html">Creative life</a>
+                        </h4>
+                        <div className="entity-category">
+                            <a className="content-link" href="news-blocks-sidebar-right.html">comedy</a>,
+                            <a className="content-link" href="news-blocks-sidebar-right.html">detective</a>,
+                            <a className="content-link" href="news-blocks-sidebar-right.html">sci-fi</a>
+                        </div>
+                        <p className="text-short entity-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ultrices justo a pellentesque. Praesent venenatis dolor nec tempus lacinia. Donec ac condimentum dolor. Nullam sit amet nisl hendrerit, pharetra nulla convallis, malesuada diam. Donec ornare nisl eu lectus rhoncus, at malesuada metus rutrum. Aliquam a nisl vulputate, sodales ipsum aliquam, tempus purus. Suspendisse convallis, lectus nec vehicula sollicitudin, lorem sapien rhoncus dolor, vel lacinia urna velit ullamcorper nisi. Phasellus pellentesque, magna nec gravida feugiat, est magna suscipit ligula, vel porttitor nunc enim at nibh. Sed varius sit amet leo vitae consequat.
+                        </p>
+                        <div className="entity-actions">
+                            <a className="text-uppercase" href="article-sidebar-right.html">Read More</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div className="col-md-6">
+                <article className="article-tape-entity">
+                    <a className="entity-preview" href="article-sidebar-right.html" data-role="hover-wrap">
+                        <span className="embed-responsive embed-responsive-16by9">
+                            <img className="embed-responsive-item" src="http://via.placeholder.com/720x405" alt="" />
+                        </span>
+                        <span className="entity-date">
+                            <span className="tape-block tape-horizontal tape-single bg-theme text-white">
+                                <span className="tape-dots"></span>
+                                <span className="tape-content m-auto">15 jun 2019</span>
+                                <span className="tape-dots"></span>
+                            </span>
+                        </span>
+                        <span className="d-over bg-black-80 collapse animated faster" data-show-classname="fadeIn show" data-hide-classname="fadeOut show">
+                            <span className="m-auto"><i className="fas fa-search"></i></span>
+                        </span>
+                    </a>
+                    <div className="entity-content">
+                        <h4 className="entity-title">
+                            <a className="content-link" href="article-sidebar-right.html">One step to the end</a>
+                        </h4>
+                        <div className="entity-category">
+                            <a className="content-link" href="news-blocks-sidebar-right.html">drama</a>,
+                            <a className="content-link" href="news-blocks-sidebar-right.html">superhero</a>
+                        </div>
+                        <p className="text-short entity-text">Vivamus dolor ex, viverra ut facilisis et, euismod et quam. Aliquam sit amet mattis velit, ullamcorper venenatis magna. Aenean ac maximus magna. Proin pharetra venenatis tortor, ac suscipit est ultrices vitae. Mauris vulputate, nisl in lacinia dignissim, libero justo vehicula arcu, a porttitor quam erat ac dui. Suspendisse potenti. Maecenas sit amet interdum sem. Vestibulum sit amet volutpat mauris, ut gravida velit. Donec ultricies, eros ut finibus volutpat, enim ligula tempus enim, non bibendum libero tellus at velit. Aenean placerat egestas ullamcorper.
+                        </p>
+                        <div className="entity-actions">
+                            <a className="text-uppercase" href="article-sidebar-right.html">Read More</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div className="col-md-6">
+                <article className="article-tape-entity">
+                    <a className="entity-preview" href="article-sidebar-right.html" data-role="hover-wrap">
+                        <span className="embed-responsive embed-responsive-16by9">
+                            <img className="embed-responsive-item" src="http://via.placeholder.com/720x405" alt="" />
+                        </span>
+                        <span className="entity-date">
+                            <span className="tape-block tape-horizontal tape-single bg-theme text-white">
+                                <span className="tape-dots"></span>
+                                <span className="tape-content m-auto">24 may 2019</span>
+                                <span className="tape-dots"></span>
+                            </span>
+                        </span>
+                        <span className="d-over bg-black-80 collapse animated faster" data-show-classname="fadeIn show" data-hide-classname="fadeOut show">
+                            <span className="m-auto"><i className="fas fa-search"></i></span>
+                        </span>
+                    </a>
+                    <div className="entity-content">
+                        <h4 className="entity-title">
+                            <a className="content-link" href="article-sidebar-right.html">Here we go again</a>
+                        </h4>
+                        <div className="entity-category">
+                            <a className="content-link" href="news-blocks-sidebar-right.html">romance</a>,
+                            <a className="content-link" href="news-blocks-sidebar-right.html">historical</a>
+                        </div>
+                        <p className="text-short entity-text">In luctus ac nisi vel vulputate. Sed blandit augue ut ex eleifend, ac posuere dolor sollicitudin. Mauris tempus euismod mauris id semper. Vestibulum ut vulputate elit, id ultricies libero. Aenean laoreet mi augue, at iaculis nisi aliquam eu. Quisque nec ipsum vehicula diam egestas porttitor eu vitae ex. Curabitur auctor tortor elementum leo faucibus, sit amet imperdiet ante maximus. Nulla viverra tortor dignissim purus placerat dapibus nec ut orci. Quisque efficitur nulla quis pulvinar dapibus. Phasellus sodales tortor sit amet sagittis condimentum. Donec ac ultricies ex. In odio leo, rhoncus aliquam bibendum sit amet, varius sit amet nisl.
+                        </p>
+                        <div className="entity-actions">
+                            <a className="text-uppercase" href="article-sidebar-right.html">Read More</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div className="col-md-6">
+                <article className="article-tape-entity">
+                    <a className="entity-preview" href="article-sidebar-right.html" data-role="hover-wrap">
+                        <span className="embed-responsive embed-responsive-16by9">
+                            <img className="embed-responsive-item" src="http://via.placeholder.com/720x405" alt="" />
+                        </span>
+                        <span className="entity-date">
+                            <span className="tape-block tape-horizontal tape-single bg-theme text-white">
+                                <span className="tape-dots"></span>
+                                <span className="tape-content m-auto">11 may 2019</span>
+                                <span className="tape-dots"></span>
+                            </span>
+                        </span>
+                        <span className="d-over bg-black-80 collapse animated faster" data-show-classname="fadeIn show" data-hide-classname="fadeOut show">
+                            <span className="m-auto"><i className="fas fa-search"></i></span>
+                        </span>
+                    </a>
+                    <div className="entity-content">
+                        <h4 className="entity-title">
+                            <a className="content-link" href="article-sidebar-right.html">The one and the other</a>
+                        </h4>
+                        <div className="entity-category">
+                            <a className="content-link" href="news-blocks-sidebar-right.html">drama</a>,
+                            <a className="content-link" href="news-blocks-sidebar-right.html">western</a>,
+                            <a className="content-link" href="news-blocks-sidebar-right.html">sci-fi</a>
+                        </div>
+                        <p className="text-short entity-text">Aenean molestie turpis eu aliquam bibendum. Nulla facilisi. Vestibulum quis risus in lorem suscipit tempor. Morbi consectetur enim vitae justo finibus consectetur. Mauris volutpat nunc dui, quis condimentum dolor efficitur et. Phasellus rhoncus porta nunc eu fermentum. Nullam vitae erat hendrerit, tempor arcu eget, eleifend tortor.
+                        </p>
+                        <div className="entity-actions">
+                            <a className="text-uppercase" href="article-sidebar-right.html">Read More</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div className="section-bottom">
+            <a className="btn btn-theme" href="news-blocks-sidebar-right.html">View All News</a>
+        </div>
+    </div>
+</section>
+}
