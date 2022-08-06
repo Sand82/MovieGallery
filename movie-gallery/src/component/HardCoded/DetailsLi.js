4749aef1dafef0ff9d4a1640d8e4ccd50deda575
Sand82 <sandoki@abv.bg>
2022-08-06 10:30:21 +0300

    introduced comments section in client side

---
+++ movie-gallery/src/component/HardCoded/DetailsLi.js
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+
+const DetailsLi = () => {
+  return (
+    <>
+      <li>
+        <span className="entity-list-title">Release:</span>July 21, 2014 (Dolby
+        Theatre), August 1, 2014 (United States)
+      </li>
+      <li>
+        <span className="entity-list-title">Directed:</span>
+        <Link className="content-link" to="#">
+          Lindson Wardens
+        </Link>
+        ,
+        <Link className="content-link" to="#">
+          Anabelle One
+        </Link>
+      </li>
+      <li>
+        <span className="entity-list-title">Starring:</span>
+        <Link className="content-link" to="#">
+          Octopus Wardens
+        </Link>
+        ,
+        <Link className="content-link" to="#">
+          Quanta Wardens
+        </Link>
+        ,
+        <Link className="content-link" to="#">
+          Anabelle Two
+        </Link>
+        ,
+        <Link className="content-link" to="#">
+          Anabelle Three
+        </Link>
+      </li>
+      <li>
+        <span className="entity-list-title">Production company:</span>
+        <Link className="content-link" to="#">
+          Re-Production Bro.
+        </Link>
+        ,
+        <Link className="content-link" to="#">
+          Pentakid
+        </Link>
+      </li>
+      <li>
+        <span className="entity-list-title">Country:</span>
+        <Link className="content-link" to="#">
+          USA
+        </Link>
+        ,
+        <Link className="content-link" to="#">
+          India
+        </Link>
+      </li>
+      <li>
+        <span className="entity-list-title">Language:</span>
+        english
+      </li>
+    </>
+  );
+};
+
+export default DetailsLi;
