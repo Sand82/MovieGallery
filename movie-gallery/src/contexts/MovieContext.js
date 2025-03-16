428db63be8003dab734f4fbd37a4203e8e24ba0a
Sand82 <sandoki@abv.bg>
2025-03-17 01:14:35 +0200

    fixed small bugs

--- movie-gallery/src/contexts/MovieContext.js
+++ movie-gallery/src/contexts/MovieContext.js
@@ -27,14 +27,12 @@ const movieReducer = (state, action) => {
       return [action.payload, ...state];
 
     case EDIT_MOVIE:
-      return [
-        ...state.filter((movie) =>
-          movie.id == action.payload.id ? action.payload : movie
-        ),
-      ];
+      return state.map((movie) =>
+        movie.id == action.payload.id ? action.payload : movie
+      );
 
     case DELETE_MOVIE:
-      return [...state.filter((movie) => movie.id != action.payload)];
+      return state.filter((movie) => movie.id != action.payload);
 
     default:
       return state;
