79ae690a887f749fb7285e75b226ff25c1d2ad6b
Sand82 <sandoki@abv.bg>
2025-05-24 18:49:44 +0300

    added tag styles and remove functionality

--- movie-gallery/src/component/UI/Tags/Tag/Tag.jsx
+++ movie-gallery/src/component/UI/Tags/Tag/Tag.jsx
@@ -2,16 +2,17 @@ import styles from "./Tag.module.css"
 
 const Tag = ({value, tagRemoveHandler}) => {
 
-	const removeHandler = () => {
-		tagRemoveHandler(value);
-	}
+  const removeHandler = () => {
+    
+    tagRemoveHandler(value);
+  }
 
-	return (
-		<span className={styles["tag-container"]}>
-		 	<span className={` ${styles["tag-value"]}`}>{value}</span>
-			<span className={styles["tag-remove"]} onClick={removeHandler}><i class="fa-solid fa-trash"></i></span>
-		</span>
-	)
+  return (
+    <span className={styles["tag-container"]}>
+       <span className={` ${styles["tag-value"]}`}>{value}</span>
+      <span className={styles["tag-remove"]} onClick={removeHandler}><i class="fa-solid fa-trash"></i></span>
+    </span>
+  )
 }
 
 export default Tag;
