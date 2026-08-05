56f85b4aae4934f5ce041d34a5d5fb030b9a0a1b
Sand82 <sandoki@abv.bg>
2026-08-05 23:44:45 +0300

    added new controller for passing scraped movies

---
+++ server/MovieGalleryWebAPI/Controllers/MovieImportController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieGalleryWebAPI.Models.Movies;
+using MovieGalleryWebAPI.Models.StaticData;
+
+namespace MovieGalleryWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MovieImportController : ControllerBase
+    {
+        //private readonly IMovieImportService movieImportService;
+
+        public MovieImportController()
+        {
+            //this.movieImportService = movieImportService;
+        }
+
+        [HttpPost("import")]
+        public async Task<IActionResult> Import([FromBody] List<MovieScraperData> movies)
+        {
+            //await _movieImportService.ImportAsync(movies);
+            ;
+
+            return Ok();
+        }
+    }
+}
---
+++ server/MovieGalleryWebAPI/Models/StaticData/MovieScraperData.cs
@@ -0,0 +1,51 @@
+﻿namespace MovieGalleryWebAPI.Models.StaticData
+{
+    public class MovieScraperData
+    {
+        public MovieScraperData()
+        {
+
+            this.MovieStarrings = new HashSet<string>();
+
+            this.MovieDirectors = new HashSet<string>();           
+
+            this.MovieLanguages = new HashSet<string>();
+
+            this.MovieCategories = new HashSet<string>();
+
+            this.MovieTags = new HashSet<string>();
+        }
+
+        public string? ExternalId { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? BackgroundImage { get; set; }
+
+        public string? MainImage { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Year { get; set; }
+
+        public string? Duration { get; set; }
+
+        public string? EmbededVideo { get; set; }
+
+        public string? Release { get; set; }
+
+        public string? Company { get; set; }
+
+        public bool IsDelete { get; set; } = false;
+
+        public ICollection<string>? MovieTags { get; set; }
+
+        public ICollection<string>? MovieStarrings { get; set; }
+
+        public ICollection<string>? MovieDirectors { get; set; }        
+
+        public ICollection<string>? MovieLanguages { get; set; }
+
+        public ICollection<string> MovieCategories { get; set; }
+    }
+}
